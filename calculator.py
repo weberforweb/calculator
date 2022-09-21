d899042f471852a50423d4256c7ae1ab3e9213e0
Weber for Web <weberforweb@gmail.com>
2022-09-21 18:44:34 -0400

    add venv to gitignore

--- calculator.py
+++ calculator.py
@@ -5,5 +5,14 @@ window = tk.Tk()
 
 window.title('My Calculator')
 
+lbl_result_screen = tk.Label(
+    master = window,
+    text = "Results"
+)
+
+
+
+lbl_result_screen.grid(row = 0, column = 0)
+
 
 window.mainloop()
