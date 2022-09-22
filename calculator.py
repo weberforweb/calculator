7bb54718cee5c2665b7574df8875dae71fabd368
Weber for Web <weberforweb@gmail.com>
2022-09-21 20:22:40 -0400

    putting all button creation in one loop

--- calculator.py
+++ calculator.py
@@ -15,40 +15,22 @@ lbl_result_screen = tk.Label(
     height = 5
 )
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
-math_functions = ['+', '-', '*']
-last_row_items = ['.', 0, 'c', '=']
+button_items = [1, 2, 3,'+', 4, 5, 6, '-', 7, 8, 9,'*','.', 0, 'C', '=']
 
-
-# def create_button(numbers_and_symbols):
-for num in numbers:
-    btn_numbers = ttk.Button(
+# def create_button(button_items_and_symbols):
+for items in button_items:
+    btn_button_items = ttk.Button(
         master = window,
-        text = num,
+        text = items,
     )    
-    if num <= 3:        
-        btn_numbers.grid(row = 1, column = num -1, sticky = (W,))
-    elif num > 3 and num <= 6:        
-        btn_numbers.grid(row = 2, column = num-4, sticky = (W,))
+    if items in range(1,4) or items == '+':        
+        btn_button_items.grid(row = 1, column = button_items.index(items), sticky = (W,))
+    elif items in range(4,7) or items == '-':        
+        btn_button_items.grid(row = 2, column = button_items.index(items) -4, sticky = (W,))
+    elif items in range(7,10) or items == '*':
+        btn_button_items.grid(row = 3, column = button_items.index(items) -8, sticky = (W,))
     else:
-        btn_numbers.grid(row = 3, column = num -7, sticky = (W,))
-
-for symbols in math_functions:
-    btn_symbols = ttk.Button(
-        master = window,
-        text = symbols,
-    )   
-    btn_symbols.grid(row = (math_functions.index(symbols)+1), column = 3 )
-
-for item in last_row_items:
-    btn_last_row_items = ttk.Button(
-        master = window,
-        text = item
-    )
-    btn_last_row_items.grid(row = 4, column = last_row_items.index(item))
-
-
-
+        btn_button_items.grid(row = 4, column = button_items.index(items) -12, sticky = (W,), pady = (0,5))
 
 
 lbl_result_screen.grid(row = 0, column = 0)
