70d4a1bbf11740b9a306dcdc2e46b34f4600f8d7
Weber for Web <weberforweb@gmail.com>
2022-09-28 17:41:31 -0400

    improvement button generator module by enumerate

--- button_generator_2.py
+++ button_generator_2.py
@@ -2,13 +2,10 @@
 
 from tkinter import ttk
 
-# window = tk.Tk()
-
 def button_generator(button_dic):
-    for item in button_dic:
+    for i, item in enumerate(button_dic):
         button_item = ttk.Button(
-                # master = window,
                 text = item,
                 # command = print('hello')
             )
-        button_item.grid (row = (button_dic.index(item) // 4) + 1, column = button_dic.index(item) %4)
+        button_item.grid (row = (i // 4) + 1, column = i %4)
--- calculator_2.py
+++ calculator_2.py
@@ -5,20 +5,20 @@ import tkinter as tk
 from button_generator_2 import button_generator
 
 window = tk.Tk()
+window.title('MY CALCULATOR')
 
-button_dic = ['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '.', '0', '=', '**', '/', 'C', '//', '%']
+BUTTON_DIC = ['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '.', '0', '%', '**', '/', 'C', '//', '=']
+
 
 result_box = tk.Label(
     master = window,
     height = 3,
+    # text = lambda button_generator('command')
 )
 
 result_box.grid (row = 0, column = 0)
 
 
 
-
-
-
-button_generator(button_dic)
+button_generator(BUTTON_DIC)
 window.mainloop()
